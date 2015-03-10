370b62c42d4e00cdb834491d3a817bc17e17f380
Scott Lobdell <slobdell@hearsaycorp.com>
2015-03-09 17:58:43 -0700

    always post to sandbox

--- video_editing/basic_navigation/api.py
+++ video_editing/basic_navigation/api.py
@@ -6,7 +6,7 @@ from django.http import Http404
 from django.http import HttpResponse
 
 
-if os.environ.get("I_AM_IN_DEV_ENV"):
+if True or os.environ.get("I_AM_IN_DEV_ENV"):
     AMAZON_HOST = "https://workersandbox.mturk.com/mturk/externalSubmit"
 else:
     AMAZON_HOST = "https://www.mturk.com/mturk/externalSubmit"
